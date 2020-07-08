0eb319803e3039d5646c3d545fccd1d9c65a365a
ngkang917 <58020069+ngkang917@users.noreply.github.com>
2020-07-09 07:58:05 +0900

    주석 제거

---
+++ APPROVAL/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace APPROVAL.Models
+{
+    class Category{
+    [key]
+    public int id { get; set; }
+    public string name{ get; set; }
+    public string language { get; set; }
+    public int ordered { get; set; }
+}
+}
