a601c5035091585e85a8acdb1eb670585fa4bc80
강남구B <es-namgukim@smilegate.com>
2020-07-15 18:49:13 +0900

    강남구
    db 마이그레이션 코드로 적용

---
+++ APPROVAL/Configurations/DataContextCreater.cs
@@ -0,0 +1,49 @@
+﻿using APPROVAL.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Retry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APPROVAL.Configurations
+{
+    public class MariaContextCreater
+    {
+        public async Task CreatAsync(MariaContext context, ILogger<MariaContextCreater> logger)
+        {
+            var policy = CreatePolicy(logger, nameof(MariaContextCreater));
+
+            await policy.ExecuteAsync(async () =>
+            {
+                using (context)
+                {
+                    context.Database.Migrate();
+                    await context.SaveChangesAsync();
+                }
+            });
+        }
+
+
+        private AsyncRetryPolicy CreatePolicy(ILogger<MariaContextCreater> logger, string prefix, int retries = 3)
+        {
+            string logDefine = "[{prefix}] Exception {ExceptionType} with message {Message} detected on attempt {retry} of {retries}";
+
+            return Policy.Handle<SqlException>().
+                WaitAndRetryAsync(
+                    retryCount: retries,
+                    sleepDurationProvider: retry => TimeSpan.FromSeconds(5),
+                    onRetry: (exception, timeSpan, retry, ctx) =>
+                    {
+                        logger.LogWarning(exception, logDefine, prefix, exception.GetType().Name, exception.Message, retry, retries);
+                    }
+                );
+        }
+
+    }
+}
--- APPROVAL/Program.cs
+++ APPROVAL/Program.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APPROVAL.Configurations;
+using APPROVAL.Data;
+using APPROVAL.Extentions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,9 +14,33 @@ namespace APPROVAL
 {
     public class Program
     {
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            try
+            {
+                //Log.Information("Configuring web host ({ApplicationContext})...", AppName);
+                host.MigrateDbContext<MariaContext>((context, services) =>
+                {
+                    var logger = services.GetService(typeof(ILogger<MariaContextCreater>));
+                    new MariaContextCreater().CreatAsync(context, logger as ILogger<MariaContextCreater>).Wait();
+                });
+
+                //Log.Information("Starting web host ({ApplicationContext})...", AppName);
+
+            }
+            catch (Exception ex)
+            {
+                //Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
+            }
+            finally
+            {
+                //Log.CloseAndFlush();
+                host.Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
